c291a0f645c04d6e6fcbf07d132e78998a1cd402
Jo Cranford <jo.cranford@gmail.com>
2011-09-25 14:47:19 +0100

    Added jslint and runner

---
+++ jslint-runner.js
@@ -0,0 +1,73 @@
+var options = {
+    node: true,
+    predef: [
+            "jasmine",
+            "describe",
+            "it",
+            "expect",
+            "beforeEach"
+    ]
+};
+
+var ANSIColors = {
+  pass:    function() { return '\033[32m'; }, // Green
+  fail:    function() { return '\033[31m'; }, // Red
+  neutral: function() { return '\033[0m';  }  // Normal
+};
+
+var stringWithColor = function(str, color) {
+    return (color || ANSIColors.neutral()) + str + ANSIColors.neutral();
+  }
+
+var fs = require('fs'),
+        sys = require('sys'),
+        dirName = process.argv[2],
+        readFile = function (name) {
+            return fs.readFileSync(name).toString();
+        },
+        print = function (message) {
+            sys.puts(message);
+        },
+        quit = function (code) {
+            process.exit(code)
+        },
+        jslint = require('./jslint');
+
+
+if (!dirName) {
+    print("Usage: jslint.js directory");
+    quit(1);
+}
+
+fs.readdir(process.cwd() + "/" + dirName, function(err, list) {
+    list.forEach(function (file) {
+        console.log(file)
+        var fileExtension = file.substr(file.length - 3);
+        if (fileExtension === ".js") {
+            doLint(dirName + "/" + file);
+        }
+    });
+});
+
+var doLint = function(fileName) {
+    var e,i,input;
+    input = readFile(fileName);
+    if (!input) {
+        print(stringWithColor("jslint: Couldn't open file '" + fileName + "'.", ANSIColors.fail()));
+        quit(1);
+    }
+    if (!jslint.JSLINT(input, options)) {
+        print(stringWithColor("Problems in " + fileName, ANSIColors.fail()));
+        for (i = 0; i < jslint.JSLINT.errors.length; i += 1) {
+            e = jslint.JSLINT.errors[i];
+            if (e) {
+                print('Lint at line ' + e.line + ' character ' +
+                        e.character + ': ' + e.reason);
+                print((e.evidence || '').replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
+                print('');
+            }
+        }
+    } else {
+        print(stringWithColor("jslint: No problems found in " + fileName, ANSIColors.pass()));
+    }
+};
